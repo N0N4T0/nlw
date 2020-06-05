7faf9d124b39e3076231b2550cf3337059ca2362
Lucio Nonato <luciononato@hotmail.com>
2020-06-05 03:42:44 -0300

    2.1 commit
    selecionando dados com javascript

--- javascript/create-point.js
+++ javascript/create-point.js
@@ -27,14 +27,16 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
         .then( (res) => res.json())
         .then( cities => {
-
-        for(const city of cities){
-            citySelect
-                .innerHTML += `<option value="${city.id}">${city.nome}</option>`
-        }
+            for(const city of cities){
+                citySelect
+                    .innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            }
 
         citySelect.disabled = false          
     })
@@ -44,3 +46,40 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+//itens de coleta
+const itensToCollect = document.querySelectorAll(".itens-grid li")    
+
+for (const item of itensToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItens = document.querySelector("input[name=itens")
+
+
+let selectedItens = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    
+    //add or remove a class with js
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItens.findIndex(item => item == itemId)
+
+    if(alreadySelected >= 0){
+        const filteredItens = selectedItens.filter(item =>{
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+
+        selectedItens = filteredItens
+    } else {
+        selectedItens.push(itemId)
+    }
+    //console.log(selectedItens)
+
+    collectedItens.value = selectedItens
+}
